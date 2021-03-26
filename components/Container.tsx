a128121281b4f73e6903228144a2573132417dc1
Victor Ajibade <ajibadevic@gmail.com>
2021-03-26 15:51:11 +0100

    Remove animation

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,5 +1,4 @@
 import { Box } from "@chakra-ui/layout";
-import { motion } from "framer-motion";
 import { ReactNode } from "react";
 import Footer from "./Footer";
 
@@ -10,15 +9,7 @@ type Props = {
 const Container = ({ children }: Props) => {
   return (
     <Box position="relative" overflow="hidden" minH={{ base: "100vh", md: "1000px" }}>
-      <motion.div
-        initial={{ y: -20, opacity: 0.5 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{
-          duration: 0.2,
-        }}
-      >
-        {children}
-      </motion.div>
+      {children}
       <Footer />
     </Box>
   );
